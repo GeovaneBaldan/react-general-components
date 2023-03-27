eeefce203be16a3d47e855791e12842c2b28e918
Geovane Baldan <geovane.baldan@gmail.com>
2023-03-26 22:38:35 -0300

    fix: fix export files command

--- src/templates/export/index.ts
+++ src/templates/export/index.ts
@@ -3,6 +3,7 @@ export function getExportedFilesTemplate(filenames: string[]) {
 
   for (const filename of filenames) {
     const name = filename.split('.')[0]
+    if (name === 'index') continue
     template += `export * from './${name}'\n`
   }
 
